6deefeda72b8aba54f0aff5fe3a8a3a253a136d6
Sergey Ilinykh <rion4ik@gmail.com>
2018-06-05 12:52:23 +0500

    Repackaging to gssapi libs and windows extra libs works now

--- sub/gssapi/conanfile.py
+++ sub/gssapi/conanfile.py
@@ -1,19 +1,26 @@
+import os
+
 from conans import ConanFile
 
-class Kerberos5WinExtra(ConanFile):
-    name = "krb5-winextra"
+class Kerberos5GssAPI(ConanFile):
+    name = "krb5-gssapi"
     version = "1.16.1"
-    build_requires = "krb5-gssapi/1.16.1@rion/testing"
+    build_requires = "krb5/1.16.1@rion/stable"
     keep_imports = True
+    license = "MIT"
+    url = "https://github.com/krb5/krb5.git"
+    description = "MIT Kerberos V: gssapi libs"
+    settings = "os", "compiler", "build_type", "arch"
 
     def imports(self):
-        for n in ["kfwlogon","krbcc","leashw","xpprof"]:
-			self.copy(n + "*.dll",   dst="bin")
-			self.copy(n + "*.so",    dst="bin")
-			self.copy(n + "*.dylib", dst="bin")
-			self.copy(n + "*.a",     dst="lib")
-			self.copy(n + "*.lib",   dst="lib")
-		
-		
+        self.copy("*.h", dst="include", src="include")
+        for n in ["comerr","gssapi","k5sprt","krb5_","wshelp"]:
+            self.copy(n + "*.dll",   dst="bin", src="bin")
+            self.copy(n + "*.so",    dst="bin", src="bin")
+            self.copy(n + "*.dylib", dst="bin", src="bin")
+            self.copy(n + "*.a",     dst="lib", src="lib")
+            self.copy(n + "*.lib",   dst="lib", src="lib")
+        
+        
     def package(self):
         self.copy("*")
--- sub/winextra/conanfile.py
+++ sub/winextra/conanfile.py
@@ -1,20 +1,25 @@
+import os
+
 from conans import ConanFile
 
-class Kerberos5GssAPI(ConanFile):
-    name = "krb5-gssapi"
+class Kerberos5WinExtra(ConanFile):
+    name = "krb5-winextra"
     version = "1.16.1"
-    build_requires = "krb5/1.16.1@rion/testing"
+    requires = "krb5-gssapi/1.16.1@rion/stable"
+    build_requires = "krb5/1.16.1@rion/stable"
     keep_imports = True
+    license = "MIT"
+    url = "https://github.com/krb5/krb5.git"
+    description = "MIT Kerberos V: Windows extra libs"
+    settings = "os", "compiler", "build_type", "arch"
 
     def imports(self):
-        self.copy("*.h", dst="include", src=os.path.join(install_dir, "include"))
-		for n in ["comerr","gssapi","k5sprt","krb5_","wshelp"]:
-			self.copy(n + "*.dll",   dst="bin")
-			self.copy(n + "*.so",    dst="bin")
-			self.copy(n + "*.dylib", dst="bin")
-			self.copy(n + "*.a",     dst="lib")
-			self.copy(n + "*.lib",   dst="lib")
-		
-		
+        for n in ["kfwlogon","krbcc","leashw","xpprof"]:
+            self.copy(n + "*.dll",   dst="bin", src="bin")
+            self.copy(n + "*.so",    dst="bin", src="bin")
+            self.copy(n + "*.dylib", dst="bin", src="bin")
+            self.copy(n + "*.a",     dst="lib", src="lib")
+            self.copy(n + "*.lib",   dst="lib", src="lib")
+
     def package(self):
         self.copy("*")
