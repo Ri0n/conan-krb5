88203a1607a3b103ab59d70d79a77cf79f350bbc
Sergey Ilinykh <rion4ik@gmail.com>
2018-06-06 12:15:39 +0500

    Since appveyor per directory is hard to cnfigure, let's return to previous idea but with some addons

--- build.py
+++ build.py
@@ -7,11 +7,15 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name=False, pure_c=False)
     builder.run()
+    # CONAN_REFERENCE: "krb5/1.16.1"
+    name, ver = os.environ["CONAN_REFERENCE"].split("/", 1)
     with tools.chdir(os.path.join("sub", "gssapi")):
+        os.environ["CONAN_REFERENCE"] = "krb5-gssapi/" + ver
         builder = ConanMultiPackager()
         builder.add_common_builds(shared_option_name=False, pure_c=False)
         builder.run()
     with tools.chdir(os.path.join("sub", "winextra")):
+        os.environ["CONAN_REFERENCE"] = "krb5-winextra/" + ver
         builder = ConanMultiPackager()
         builder.add_common_builds(shared_option_name=False, pure_c=False)
         builder.run()
