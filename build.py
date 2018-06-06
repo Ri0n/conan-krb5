97c1f479122a675acbeadfc5a6a1642340cfeb98
Sergey Ilinykh <rion4ik@gmail.com>
2018-06-06 12:10:44 +0500

    Revert "Put appveyor in subdirectories. It doesn't work otherwise"
    This reverts commit fc76905a1e6ad9cec2c4c5239c00df22cc4d9045.

--- build.py
+++ build.py
@@ -1,7 +1,17 @@
+import os
+
 from conan.packager import ConanMultiPackager
+from conans import tools
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name=False, pure_c=False)
     builder.run()
-
+    with tools.chdir(os.path.join("sub", "gssapi")):
+        builder = ConanMultiPackager()
+        builder.add_common_builds(shared_option_name=False, pure_c=False)
+        builder.run()
+    with tools.chdir(os.path.join("sub", "winextra")):
+        builder = ConanMultiPackager()
+        builder.add_common_builds(shared_option_name=False, pure_c=False)
+        builder.run()
