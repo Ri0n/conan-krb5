c3b20fe617aa915f4901783b912287ed7f1cabcf
Sergey Ilinykh <rion4ik@gmail.com>
2018-06-07 00:20:09 +0300

    Limit rutimes to just MD and MDd since we always build shared

--- build.py
+++ build.py
@@ -4,18 +4,19 @@ from conan.packager import ConanMultiPackager
 from conans import tools
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    runtimes = ["MD", "MDd"]
+    builder = ConanMultiPackager(visual_runtimes=runtimes)
     builder.add_common_builds(shared_option_name=False, pure_c=False)
     builder.run()
     # CONAN_REFERENCE: "krb5/1.16.1"
     name, ver = os.environ["CONAN_REFERENCE"].split("/", 1)
     with tools.chdir(os.path.join("sub", "gssapi")):
         os.environ["CONAN_REFERENCE"] = "krb5-gssapi/" + ver
-        builder = ConanMultiPackager()
+        builder = ConanMultiPackager(visual_runtimes=runtimes)
         builder.add_common_builds(shared_option_name=False, pure_c=False)
         builder.run()
     with tools.chdir(os.path.join("sub", "winextra")):
         os.environ["CONAN_REFERENCE"] = "krb5-winextra/" + ver
-        builder = ConanMultiPackager()
+        builder = ConanMultiPackager(visual_runtimes=runtimes)
         builder.add_common_builds(shared_option_name=False, pure_c=False)
         builder.run()
